99760faa6511716a6374ef49d45360763c39e654
Ruslan Rakhmetov <rahmetov19112003@gmail.com>
2025-06-08 10:37:30 +0500

    modified

---
+++ Cats/Data/CatsDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Cats.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Cats.Data
+{
+    public class CatsDbContext : DbContext
+    {
+        public CatsDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Cat> Catt { get; set; }
+    }
+}
--- Cats/Program.cs
+++ Cats/Program.cs
@@ -1,3 +1,6 @@
+using Cats.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<CatsDbContext>(options =>
+options.UseInMemoryDatabase("CattDb"));
 
 var app = builder.Build();
 

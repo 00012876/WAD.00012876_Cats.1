e7099e172664e707221a5bc2a4e2bfae694f5fa3
Ruslan Rakhmetov <rahmetov19112003@gmail.com>
2025-06-08 15:01:03 +0500

    started to add controllers

---
+++ Cats/Controllers/CattController.cs
@@ -0,0 +1,32 @@
+﻿using Cats.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cats.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CattController : ControllerBase
+    {
+        private readonly CatsDbContext dbContext;
+
+        public CattController(CatsDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public IActionResult GetAllCatt() 
+        {
+            var catt = dbContext.Catt.ToList();
+            return Ok(catt);
+        }
+
+        [HttpPost]
+        public IActionResult AddCatt()
+        {
+
+        }
+
+    }
+}
